0d638a7039dcc3570d705da9a596b11d3c972426
spidy9869 <63071549+spidy9869@users.noreply.github.com>
2021-12-04 23:59:14 +0530

    hooks used !!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React , {useState} from 'react';
+import React , {useState , useRef , useEffect} from 'react';
 import TodoList from './TodoList.js'
+
 
 function App() {
 
-  const [todo , setTodo] = useState([{ id:1 , name:"todo 1" , done:false}]);
+  const [todo , setTodo] = useState([]);
+  const [id , setId] = useState(0);
+  console.log(typeof useState());
+  const key = 'storage';
+
+
+ useEffect(()=>{
+   const strg=JSON.parse(localStorage.getItem(key));
+   if(strg)
+    {setTodo(strg);}
+ } , []);
+
+
+  useEffect(()=>{
+    localStorage.setItem(key , JSON.stringify(todo))
+
+  } , [todo]);
+
+  
+  const txt = useRef();
+
+  function handleTodo(){
+
+  const name = txt.current.value;
+  
+  setId(n => {
+
+    return  ++n;
+  })
+  setTodo(arr => {
+    return [ ...arr , {id:id , name : name , done:false}]
+  });
+  txt.current.value = null;
+
+
+ 
+
+    
+
+ 
+   }
 
 
 
@@ -13,8 +54,9 @@ function App() {
   <>
    <TodoList  todos = {todo}/>
    
-   <input type="text"></input>
-   <button>Add todo</button>
+   <input ref={txt} type="text"></input>
+   
+   <button onClick={handleTodo}>Add todo</button>
    <button>Clear</button>
    <div>0 todo left</div>
   </>
